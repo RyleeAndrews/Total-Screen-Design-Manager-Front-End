1bc6c0af1f3b6cdea15462994d4b968fb851d420
Rylee Andrews <ryleeandrews73@gmail.com>
2018-07-02 16:49:57 -0600

    working on the home page

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,11 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import Home from './home/home.js';
+import ScreenPrint from './home/screenprint.js';
+import DTG from './home/dtg.js';
+import Embroidered from './home/embroidered.js';
+import Vinyl from './home/vinyl.js';
+import GraphicDesign from './home/graphicdesign.js';
 class App extends React.Component {
 
   constructor(props) {
@@ -15,6 +20,11 @@ class App extends React.Component {
 
         <main>
           <Route exact path='/' component={Home} />
+          <Route exact path='/screenprint' component={ScreenPrint} />
+          <Route exact path='/dtg' component={DTG} />
+          <Route exact path='/embroidered' component={Embroidered} />
+          <Route exact path="/vinyl" component={Vinyl} />
+          <Route exact path="/graphicdesign" component={GraphicDesign} />
         </main>
       </React.Fragment>
     );
